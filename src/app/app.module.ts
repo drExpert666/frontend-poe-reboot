92e151be3118a2cf1a5703695aa605f26f6dab48
drexpert666 <sashkaro@gmail.com>
2022-02-06 02:52:16 +0300

    - корректировки дизайна

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CamerasComponent } from './views/cameras/cameras.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CamerasComponent} from './views/cameras/cameras.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {CHANNEL_URL_TOKEN} from "./data/implementation/ChannelService";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatTableModule} from "@angular/material/table";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { ServersComponent } from './views/servers/servers.component';
+import {ServersComponent} from './views/servers/servers.component';
 import {SERVER_URL_TOKEN} from "./data/implementation/ServerService";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
@@ -18,24 +18,23 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
-import { EditChannelDialogComponent } from './dialog/edit-channel-dialog/edit-channel-dialog.component';
+import {EditChannelDialogComponent} from './dialog/edit-channel-dialog/edit-channel-dialog.component';
 import {MatDialogModule} from "@angular/material/dialog";
 import {SWITCH_URL_TOKEN} from "./data/implementation/SwitchService";
 import {MatExpansionModule} from "@angular/material/expansion";
 import {MatRadioModule} from "@angular/material/radio";
-import { EditCameraDialogComponent } from './dialog/edit-camera-dialog/edit-camera-dialog.component';
+import {EditCameraDialogComponent} from './dialog/edit-camera-dialog/edit-camera-dialog.component';
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatSelectModule} from "@angular/material/select";
 import {REBOOT_URL_TOKEN} from "./data/implementation/RebootService";
-import { ConfirmDialogComponent } from './dialog/confirm-dialog/confirm-dialog.component';
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {ConfirmDialogComponent} from './dialog/confirm-dialog/confirm-dialog.component';
 import {NgxSpinnerModule} from "ngx-spinner";
-import { AfterConfirmRebootDialogComponent } from './dialog/after-confirm-reboot-dialog/after-confirm-reboot-dialog.component';
+import {AfterConfirmRebootDialogComponent} from './dialog/after-confirm-reboot-dialog/after-confirm-reboot-dialog.component';
 import {MatSortModule} from "@angular/material/sort";
 import {AuthInterceptorService} from "./helper/auth-interceptor.service";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 import {ErrorInterceptorService} from "./helper/error-interceptor.service";
-import { LoginComponent } from './auth/login/login.component';
+import {LoginComponent} from './auth/login/login.component';
 import {AppRoutingModule} from "./app.routing.module";
 import {NavigationComponent} from "./layout/navigation/navigation.component";
 import {MatCardModule} from "@angular/material/card";
@@ -114,4 +113,5 @@ import {MatCardModule} from "@angular/material/card";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/dialog/after-confirm-reboot-dialog/after-confirm-reboot-dialog.component.ts
+++ src/app/dialog/after-confirm-reboot-dialog/after-confirm-reboot-dialog.component.ts
@@ -22,20 +22,32 @@ export class AfterConfirmRebootDialogComponent implements OnInit {
   ngOnInit(): void {
     this.tmpRebootValues = this.data;
     console.log(this.tmpRebootValues);
-    if (this.tmpRebootValues.switchIp) {
+    if (this.tmpRebootValues.switchIp == 'OK') {
       this.tmpTitle = this.tmpRebootValues.switchIp;
       console.log(this.tmpRebootValues);
       this.wasError = false;
     }
+
+    if (this.tmpRebootValues.switchIp == 'PORT_NOT_FOUND') {
+      console.log(this.tmpRebootValues);
+      this.wasError = true;
+    }
+
+
+    if (this.tmpRebootValues.switchIp == 'ERROR') {
+      console.log(this.tmpRebootValues);
+      this.wasError = true;
+    }
+
     else {
-      this.tmpTitle = 'Произошла ошибка!'
+      console.log(this.tmpRebootValues);
       this.wasError = true;
     }
 
   }
 
   ok() {
-    if (this.tmpTitle == 'Произошла ошибка!') {
+    if (this.wasError) {
       this.dialogRef.close(new ActionsResult(Actions.ERROR));
     } else {
       this.dialogRef.close(new ActionsResult(Actions.OK));
